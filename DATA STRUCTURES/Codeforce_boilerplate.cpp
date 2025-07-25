5ff023f7d222ad82263170bfb8aa446a58db9ba6
PiercedBySanity1410 <oneeyedreaper1410@gmail.com>
2025-07-25 17:29:38 +0530

    Update BoilerPlate

--- DATA STRUCTURES/Codeforce_boilerplate.cpp
+++ DATA STRUCTURES/Codeforce_boilerplate.cpp
@@ -13,13 +13,13 @@ const int MOD = 1e9 + 7;
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 // Loop
-#define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
-#define FOR2(i, a) for (ll i = 0; i < ll(a); ++i)
-#define FOR3(i, a, b) for (ll i = a; i < ll(b); ++i)
-#define FOR4(i, a, b, c) for (ll i = a; i < ll(b); i += (c))
-#define FOR1_R(a) for (ll i = (a)-1; i >= ll(0); --i)
-#define FOR2_R(i, a) for (ll i = (a)-1; i >= ll(0); --i)
-#define FOR3_R(i, a, b) for (ll i = (b)-1; i >= ll(a); --i)
+#define FOR1(a) for (int _ = 0; _ < a; ++_)
+#define FOR2(i, a) for (int i = 0; i < a; ++i)
+#define FOR3(i, a, b) for (int i = a; i < b; ++i)
+#define FOR4(i, a, b, c) for (int i = a; i < b; i += (c))
+#define FOR1_R(a) for (int i = (a)-1; i >= 0; --i)
+#define FOR2_R(i, a) for (int i = (a)-1; i >= 0; --i)
+#define FOR3_R(i, a, b) for (int i = (b)-1; i >= a; --i)
 #define overload4(a, b, c, d, e, ...) e
 #define overload3(a, b, c, d, ...) d
 #define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
